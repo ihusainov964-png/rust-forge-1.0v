57cc86cc0f6ad11f920a708a6fc7693cea215a73
Lens <ihusainov964@gmail.com>
2026-07-13 00:38:16 +0300

    Add files via upload

--- src/ui/tabs/mod.rs
+++ src/ui/tabs/mod.rs
@@ -2,12 +2,14 @@
 
 pub mod launch;
 pub mod graphics;
+pub mod advanced;
 pub mod system;
 pub mod profiles;
 pub mod about;
 
 pub use launch::draw_launch_tab;
 pub use graphics::draw_graphics_tab;
+pub use advanced::draw_advanced_tab;
 pub use system::draw_system_tab;
 pub use profiles::draw_profiles_tab;
 pub use about::draw_about_tab;
